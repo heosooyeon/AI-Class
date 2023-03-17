6bcdfc79cc75aa254b7a307ec49a2052f4813f71
heosooyeon <80025144+heosooyeon@users.noreply.github.com>
2023-03-17 11:10:46 +0900

    First - Second Class Time

---
+++ Operation.py
@@ -0,0 +1,9 @@
+width = 3
+height = 5
+RectArea = width * height
+TriangleArea = width*height/2
+
+print(RectArea)
+print(width, '*', height, '=', RectArea)
+
+print('{0} : {1} * {2} / 2 = {3}'.format('Triangle Area', width, height, TriangleArea))
