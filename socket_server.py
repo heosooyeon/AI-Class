07836211b27bd15e7c6533988f3682dc143edb8a
heosooyeon <80025144+heosooyeon@users.noreply.github.com>
2023-03-24 11:44:09 +0900

    Four Study

---
+++ socket_server.py
@@ -0,0 +1,34 @@
+""" 소켓 """ 
+
+from socket import *
+
+host = '10.150.149.52'
+port = 9999
+
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.setsocket(SOL_SOCKET, SO_REUSEADDR, 1)
+
+server_socket.bind((host, port))
+
+print('linsting...')
+server_socket.listen()
+
+client_socket, addr = server_socket.accept()
+
+print('connected by', addr)
+
+cnt = 0
+
+while True:
+    data = client_socket.recv(1024)
+
+    if not data:
+        break
+
+    cnt += 1
+    print(cnt, ':: received from', addr, data.decode())
+
+    client_socket.sendall(data)
+
+client_socket.close()
+server_socket.close()
